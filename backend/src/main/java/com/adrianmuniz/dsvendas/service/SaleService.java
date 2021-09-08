06020e4b12dcb9f615cc5f1c74892e30f18f13b4
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2021-09-08 16:21:22 -0300

    Group by search

--- backend/src/main/java/com/adrianmuniz/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/adrianmuniz/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.adrianmuniz.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.adrianmuniz.dsvendas.dto.SaleDTO;
+import com.adrianmuniz.dsvendas.dto.SaleSuccessDTO;
+import com.adrianmuniz.dsvendas.dto.SaleSumDTO;
 import com.adrianmuniz.dsvendas.entities.Sale;
 import com.adrianmuniz.dsvendas.repositories.SaleRepository;
 import com.adrianmuniz.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
-		}
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
 }
