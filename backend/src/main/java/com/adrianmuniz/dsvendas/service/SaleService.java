fe03da0cd4a164e07d5f87d1a5fb02042ba001f4
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2021-09-08 15:37:47 -0300

    Pagination

---
+++ backend/src/main/java/com/adrianmuniz/dsvendas/service/SaleService.java
@@ -0,0 +1,29 @@
+package com.adrianmuniz.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.adrianmuniz.dsvendas.dto.SaleDTO;
+import com.adrianmuniz.dsvendas.entities.Sale;
+import com.adrianmuniz.dsvendas.repositories.SaleRepository;
+import com.adrianmuniz.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+		}
+}
